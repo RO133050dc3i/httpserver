f71d157471e1a540d859c16f30f09a3b2627dc49
anbo@easj.dk <gitterly12>
2014-09-23 14:45:21 +0200

    .gitignore update, more filters

---
+++ httpserverTest/HttpServerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace httpserverTest
+{
+    [TestClass]
+    public class HttpServerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("Nothing is tested!");
+        }
+    }
+}
